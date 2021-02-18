714df40ed5b30ce1facd765e7fcb87c323f0424d
Vahen <chris.xelo.pico@gmail.com>
2021-02-18 18:39:49 +0100

    [Ajout dossier ressources] - Ajout des exemple de jeu de test - Ajout d'un fichier de test pour la configuration

--- backend_millenium_falcon_computer/configuration/configuration.py
+++ backend_millenium_falcon_computer/configuration/configuration.py
@@ -7,34 +7,33 @@ web_templates_dir = os.path.join(basedir, "web_C3PO/templates")
 web_static_dir = os.path.join(basedir, "web_C3PO/static")
 resource_dir = os.path.join(basedir, "resources")
 
-# Todo -> Trouver comment le parametrer au moment de la creation de l'appli
-json_config_file_location = os.path.join(resource_dir, "example1/millenium-falcon.json") # "./resources/example1/millenium-falcon.json"
-
 
 # Configuration for the app
 
 class ConfigurationApp:
-    autonomy = 6,
-    departure = "Tatooine",
-    arrival = "Endor",
-    _routes_db = "universe.db"
-    full_route_db = os.path.join(resource_dir, "example1", _routes_db)
 
-    def init_from_json(self, folder_of_db, json_data):
+    def __init__(self):
+        self.autonomy = 6,
+        self.departure = "Tatooine",
+        self.arrival = "Endor",
+        self._routes_db = "universe.db"
+        self.json_config_file_location = os.path.join(resource_dir, "configuration/millenium-falcon.json")
+        self._json_config_file_path_folder = os.path.dirname(self.json_config_file_location)
+        self.full_route_db = os.path.join(resource_dir, self._json_config_file_path_folder, self._routes_db)
+
+    def _init_from_json(self, json_data):
         # Todo -> Add security to check if values exist or not
         self.autonomy = json_data["autonomy"]
         self.departure = json_data["departure"]
         self.arrival = json_data["arrival"]
         self._routes_db = json_data["routes_db"]
-        self.full_route_db = os.path.join(resource_dir, folder_of_db, self._routes_db)
+        self.full_route_db = os.path.join(resource_dir, self._json_config_file_path_folder, self._routes_db)
 
     def init_from_json_file(self, location):
         with open(location, 'r') as jsonfile:
             json_data = json.load(jsonfile)
-            folder_of_file = os.path.dirname(location)
-            print(folder_of_file)
-
-            self.init_from_json(folder_of_file, json_data)
+            self._json_config_file_path_folder = os.path.dirname(location)
+            self._init_from_json(json_data)
 
     def __repr__(self):
         return 'Autonomy: {}\n' \
---
+++ backend_millenium_falcon_computer/configuration/test_configuration.py
@@ -0,0 +1,47 @@
+import os
+from unittest import TestCase
+
+from backend_millenium_falcon_computer.configuration.configuration import ConfigurationApp, resource_dir
+
+
+class TestConfigurationApp(TestCase):
+    def test_init_from_json_all_ok(self):
+        json_data = {
+            "autonomy": 10,
+            "departure": "Tatooine",
+            "arrival": "Endor",
+            "routes_db": "universe.db"
+        }
+        config = ConfigurationApp()
+        config._init_from_json(json_data)
+        self.assertTrue(config.autonomy == 10)
+        self.assertTrue(config.departure == "Tatooine")
+        self.assertTrue(config.arrival == "Endor")
+        self.assertTrue(config._routes_db == "universe.db")
+
+    def test_init_from_json_all_wrong_values(self):
+        json_data = {
+            "autonomy": 15,
+            "departure": "Coruscant",
+            "arrival": "Mandalor",
+            "routes_db": "universe.db"
+        }
+        config = ConfigurationApp()
+        config._init_from_json(json_data)
+        self.assertFalse(config.autonomy == 10)
+        self.assertFalse(config.departure == "Tatooine")
+        self.assertFalse(config.arrival == "Endor")
+        self.assertTrue(config._routes_db == "universe.db")
+
+    def test_init_from_json_file_all_ok(self):
+        json_config_file_location = os.path.join(resource_dir, "test_data/millenium-falcon.json")
+        json_config_file_location_folder = os.path.dirname(json_config_file_location)
+        config = ConfigurationApp()
+        config.init_from_json_file(json_config_file_location)
+        self.assertTrue(config.autonomy == 12)
+        self.assertTrue(config.departure == "Dagobah")
+        self.assertTrue(config.arrival == "Hoth")
+        self.assertTrue(config._routes_db == "universe.db")
+        self.assertTrue(config._json_config_file_path_folder == json_config_file_location_folder)
+        self.assertTrue(
+            config.full_route_db == os.path.join(resource_dir, json_config_file_location_folder, "universe.db"))
