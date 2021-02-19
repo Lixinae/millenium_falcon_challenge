1ddf716299c192303abdab3c8922afad2d727de6
Vahen <chris.xelo.pico@gmail.com>
2021-02-19 02:32:06 +0100

    [Corrections main lancement]

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from backend_millenium_falcon_computer import create_app
-
-
-def create_server():
-    return create_app()
-
+from backend_millenium_falcon_computer import app
 
 if __name__ == '__main__':
-    app = create_server()
-    app.run(host="0.0.0.0", port='80')
+    app.run()
