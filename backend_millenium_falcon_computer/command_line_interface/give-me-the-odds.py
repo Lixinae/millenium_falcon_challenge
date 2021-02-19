662e9d472e0c4207f02885de7e0832944c1a942b
Vahen <chris.xelo.pico@gmail.com>
2021-02-19 15:15:08 +0100

    [Cli] - Ajout du code pour executer la commande

--- backend_millenium_falcon_computer/command_line_interface/give-me-the-odds.py
+++ backend_millenium_falcon_computer/command_line_interface/give-me-the-odds.py
@@ -1,17 +1,32 @@
+import json
 import sys
+
+from backend_millenium_falcon_computer.configuration.configuration import config
+from backend_millenium_falcon_computer.odds_success_calculator import calculator
+
+
+def read_json_file(empire_json_file_: json):
+    with open(empire_json_file_, 'r') as jsonfile:
+        json_empire_ = json.load(jsonfile)
+    return json_empire_
+
 
 # Todo -> Cli
 # Prend 2 fichier en entrée (les 2 json) give-me-the-odds example1/millenium-falcon.json example1/empire.json
-from backend_millenium_falcon_computer import ConfigurationApp
-
 if __name__ == '__main__':
     argv = sys.argv[1]
 
-    config = ConfigurationApp()
-    config.init_from_json_file()
+    # Todo -> Remplacer par arguments ligne de commande
+    millenium_json_config_file = "example1/millenium-falcon.json"
+    empire_json_file = "example1/empire.json"
+
+    config.init_from_json_file(millenium_json_config_file)
+    json_empire = read_json_file(empire_json_file)
+
+    odds_of_success = calculator.calculate_odds_of_success(json_empire)
+    print(odds_of_success)
     # Todo -> Read files
     # example1/millenium-falcon.json -> Config file: need to read it
     # example1/empire.json
     # Todo -> Calculate proba -> This will be done in the backend part
     # Todo -> Print result
-    pass
