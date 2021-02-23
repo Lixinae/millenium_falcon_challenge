05fabcc027328d91deb239a5a689c5208cb16443
Vahen <chris.xelo.pico@gmail.com>
2021-02-23 17:51:45 +0100

    [Database] - Ajout d'une fonction pour override les valeurs par defauts au moment de l'initialisation

--- backend_millenium_falcon_computer/database/__init__.py
+++ backend_millenium_falcon_computer/database/__init__.py
@@ -4,11 +4,10 @@ from sqlalchemy.orm import sessionmaker
 
 from backend_millenium_falcon_computer.configuration.configuration import config
 
-
+# Default values
 engine = create_engine(config.sql_alchemy_database_url, connect_args={"check_same_thread": False})
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 session = Session()
 
 # Initialisation des tables de la BDD
@@ -16,3 +15,19 @@ session = Session()
 from backend_millenium_falcon_computer.database import models
 models.Base.metadata.create_all(bind=engine)
 
+
+def init_db():
+    """
+    We need to run this after the initialisation of the configuration to override the default values
+    :return:
+    """
+    global engine, Session, Base, session
+    print(config.sql_alchemy_database_url)
+    engine = create_engine(config.sql_alchemy_database_url, connect_args={"check_same_thread": False})
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    Base = declarative_base()
+    session = Session()
+    # Initialisation des tables de la BDD
+    # On laisse le from ici pour eviter les import circulaire
+    from backend_millenium_falcon_computer.database import models
+    models.Base.metadata.create_all(bind=engine)
