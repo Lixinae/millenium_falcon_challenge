45af4e21dcce4147f6225fe3464e44dae04eab1a
Vahen <chris.xelo.pico@gmail.com>
2021-02-24 18:49:19 +0100

    [Front end - Ajout du css] - Ajout de font specifique - Ajout d'un fond - Ajout d'une favicon

--- web_C3PO/static/scripts/base_app.js
+++ web_C3PO/static/scripts/base_app.js
@@ -16,8 +16,8 @@ const base_app_vue = new Vue({
     methods: {
         submit_file() {
             let formData = new FormData();
-            formData.append('file', this.upload_file);
-            let post_url = $("#submit_file_and_comput_data_form").attr("action");
+            formData.append('file', this.input_fields.upload_file);
+            let post_url = Flask.url_for("api_bp.upload_file_api")
             let self = this;
             axios.post(post_url,
                 formData, {
@@ -34,30 +34,60 @@ const base_app_vue = new Vue({
 
                 console.log('SUCCESS!!');
             }).catch(function (response) {
-                console.log(response)
+                console.error(JSON.stringify(response.message));
                 console.log('FAILURE!!');
             });
         },
         handle_file_upload() {
-            this.upload_file = this.$refs.file.files[0];
-            // console.log('>>>> 1st element in files array >>>> ', this.upload_file);
+            this.input_fields.upload_file = this.$refs.file.files[0];
         },
+        // add_file(f) {
+        //     let dropped_files = f.dataTransfer.files
+        //     if (!dropped_files){
+        //         return;
+        //     }
+        //     // On ne veut qu'un seul fichier
+        //     if (dropped_files.length > 1){
+        //         return;
+        //     }
+        //     this.input_fields.upload_file = dropped_files[0]
+        //     // dropped_files.forEach(f =>{
+        //     //     this.files.push(f);
+        //     // });
+        // },
+        // remove_file(file){
+        //     if (file === this.upload_file){
+        //         this.input_fields.upload_file = ""
+        //     }
+        // },
         add_planets_to_display() {
             if (this.answer_data.trajectory.length > 0) {
                 let trajectory_display_planets = $(".trajectory_display_planets")
                 trajectory_display_planets.html("")
+                let self = this
                 $.each(this.answer_data.trajectory, function (key, planet_name) {
                     let file_name = "img/" + planet_name + "_128.png"
-                    let url = Flask.url_for("static", {"filename": file_name})// Proviens du package JsGlue
+                    let url_img = Flask.url_for("static", {"filename": file_name})// Proviens du package JsGlue
                     let start_figure = "<figure>"
-                    let planet_img = "<img class=\"planet_img\" src=\'" + url + "\' alt=\"Planet:" + planet_name + " miniature\">"
-                    let figure_caption = "<figcaption>" + planet_name + "</figcaption>"
+                    let planet_img = "<img class=\"planet_img\" src=\'" + url_img + "\' alt=\"Planet:" + planet_name + " miniature\">"
+                    let figure_caption = "<figcaption class='fig_caption'>" + planet_name + "</figcaption>"
                     let end_figure = "</figure>"
+                    let url_arrow = Flask.url_for("static", {"filename": "img/arrow.png"})
+                    let arrow_img = "<img class=\"arrow_img\" src=\'" + url_arrow + "\' alt=\"Planet:" + planet_name + " miniature\">"
                     let output = start_figure + planet_img + figure_caption + end_figure
+
+                    if (key < self.answer_data.trajectory.length - 1) {
+                        output += arrow_img;
+                    }
                     trajectory_display_planets.append(output);
                 });
             }
         }
     },
-    delimiters: ["<%", "%>"] // delimiters specifique pour eviter les conflits avec jinja
+    delimiters: ["<%", "%>"], // delimiters specifique pour eviter les conflits avec jinja
+    computed: {
+        upload_disabled() {
+            return this.input_fields.upload_file.length === 0;
+        }
+    }
 });
